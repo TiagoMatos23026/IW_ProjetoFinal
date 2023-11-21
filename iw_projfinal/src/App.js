41fa3a0b88834819cc29d3e27e2bf6462bb5db0d
TiagoMatos23026 <128063394+TiagoMatos23026@users.noreply.github.com>
2023-11-21 16:01:26 +0000

    Mudanças na aula
    Eliminação do ficheiro ProfileInfo. Encapsulamento do JSON da API.

--- iw_projfinal/src/App.js
+++ iw_projfinal/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>ola</div>
+    <div></div>
   );
 }
 
--- iw_projfinal/src/Profile/Profile.js
+++ iw_projfinal/src/Profile/Profile.js
@@ -10,6 +10,7 @@ class Profile extends Component {
     }
 
     async getProfile(id) {
+
         var requestOptions = {
             method: 'GET',
             redirect: 'follow',
@@ -19,27 +20,28 @@ class Profile extends Component {
             },
         };
 
-        try {
+        /*try {
             const data = await fetch("https://api.sheety.co/603075854cd9316246fab517d2525742/iwProjFinal/utilizadores/" + 2, requestOptions);
             const jsonData = await data.json();
             console.log(jsonData.utilizadore);
             this.setState({ userProfile: jsonData.utilizadore });
         } catch (error) {
             console.log('error', error);
-        }
+        }*/
 
         
         fetch("https://api.sheety.co/603075854cd9316246fab517d2525742/iwProjFinal/utilizadores/" + 2, requestOptions)
-            .then(res => res.json())
+            .then(x => x.json())
             .then(json => json.utilizadore)
             .then(result => this.setState({ userProfile: result }))
             .catch(error => console.log('error', error));
     }
 
+
     render() {
         return <div>
             <button className="alert btn-warning" onClick={() => this.getProfile()}>Ver Perfil</button>
-            <h1>Nome: {this.state.userProfile.nome}</h1>
+            <h1>Nome: {this.state.userProfile.nickname}</h1>
         </div>
     }
 }
